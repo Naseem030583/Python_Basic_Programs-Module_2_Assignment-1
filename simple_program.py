184f00f98e666999624194d59cd9ad14c765ddbd
Naseem030583 <137470997+Naseem030583@users.noreply.github.com>
2025-09-30 15:15:38 -0500

    Add files via upload

---
+++ simple_program.py
@@ -0,0 +1,18 @@
+num1 = int(input("Enter first number: "))
+num2 = int(input("Enter second number: "))
+
+Addition = num1 + num2
+Subtraction = num1 - num2
+Multiplication = num1 * num2
+Division = num1 / num2
+
+print(Addition)
+print(Subtraction)
+print(Multiplication)
+print(Division)
+
+
+First_name = input("enter your first name: ")
+Second_name = input("enter your second name: ")
+Full_name = First_name + " " + Second_name
+print(f"\nHello, {Full_name}! Welcome to the Python program.")
